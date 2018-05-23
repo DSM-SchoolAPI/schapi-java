d95e538ba8461b2836066c3d9fe9874e2dd80cc9
JoMingyu <city7310@naver.com>
2018-05-23 10:41:03 +0900

    DONE: Meal

---
+++ src/datastructure/Meal.java
@@ -0,0 +1,11 @@
+package datastructure;
+
+public class Meal {
+    public String breakfast;
+    public String lunch;
+    public String dinner;
+
+    public Meal() {
+        this.breakfast = this.lunch = this.dinner = "급식이 없습니다.";
+    }
+}
