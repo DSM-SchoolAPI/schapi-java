2fd6438303361ae6d3564f1c78cf40b1b9c76c77
JoMingyu <city7310@naver.com>
2018-05-23 15:13:39 +0900

    UPDATE: Meal 클래스 변경
    - String 형태의 필드들을 ArrayList<String> 타입으로

--- src/datastructure/Meal.java
+++ src/datastructure/Meal.java
@@ -1,11 +1,13 @@
 package datastructure;
 
+import java.util.ArrayList;
+
 public class Meal {
-    public String breakfast;
-    public String lunch;
-    public String dinner;
+    public ArrayList<String> breakfast;
+    public ArrayList<String> lunch;
+    public ArrayList<String> dinner;
 
     public Meal() {
-        this.breakfast = this.lunch = this.dinner = "급식이 없습니다.";
+        this.breakfast = this.lunch = this.dinner = new ArrayList<>();
     }
 }
